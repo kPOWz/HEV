d6a247ae01d8a4d8246bbae6bb9403cd95033428
kPOWz <karrie.zeman@gmail.com>
2014-05-29 22:01:34 -0500

    added minification in prep for release

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+// var jshint = require('gulp-jshint');
+//var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-minify-css');
+
+// // Lint JS
+// gulp.task('lint', function() {
+//   return gulp.src('src/*.js')
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('default'));
+// });
+
+//Minify CSS
+gulp.task('minifyCSS', function(){
+  return gulp.src('*.css')  
+    .pipe(cssmin())
+    .pipe(rename('map.min.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+// Concat & Minify JS
+gulp.task('minifyJS', function(){
+  return gulp.src('*.js')  
+    .pipe(uglify())
+    .pipe(rename('map.min.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('*.js', ['minifyJS']);
+    gulp.watch('*.css', ['minifyCSS']);
+});
+
+// Default
+// gulp.task('default', ['lint', 'minify', 'watch']);
+gulp.task('default', ['minifyJS', 'minifyCSS']);
