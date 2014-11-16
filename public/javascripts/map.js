6eddaf93406a0b7b101fcb9aeb318799ad05a979
kPOWz <ymmez22@yahoo.com>
2014-11-15 18:48:37 -0600

    change neighborhood boundary to align with city data

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -34,8 +34,7 @@ function initialize() {
     styles: styles,
     mapTypeControl: false,
   };
-  
-  
+
   var mapCanvas = document.getElementById('map-canvas');
   map = new google.maps.Map(mapCanvas,mapOptions);
 
@@ -45,16 +44,46 @@ function initialize() {
   // Define HEV boundary
   //TODO: via KML instead  
   var hevCoords = [
-      new google.maps.LatLng(41.5905, -93.6176),
-      new google.maps.LatLng(41.5945, -93.5983),
-      new google.maps.LatLng(41.5897, -93.5968),
-      new google.maps.LatLng(41.5898, -93.5980),
-      new google.maps.LatLng(41.5862, -93.6161),
-      new google.maps.LatLng(41.5876, -93.6166),
-      new google.maps.LatLng(41.5885, -93.6169),
-      new google.maps.LatLng(41.5895, -93.6172),
-      new google.maps.LatLng(41.5900, -93.6174),
-      new google.maps.LatLng(41.5905, -93.6176)
+      new google.maps.LatLng(41.59533840000001,-93.6163902),
+      new google.maps.LatLng(41.5945761, -93.6168087),
+      new google.maps.LatLng(41.5940947, -93.6171305),
+      new google.maps.LatLng(41.59387, -93.6171734),
+      new google.maps.LatLng(41.5936855, -93.61730220000001),
+      new google.maps.LatLng(41.5924899, -93.6178815),
+      new google.maps.LatLng(41.5919042, -93.6179888),
+      new google.maps.LatLng(41.5916554, -93.6179459),
+      new google.maps.LatLng(41.5892482, -93.6172056),
+      new google.maps.LatLng(41.5891519, -93.617152),
+      new google.maps.LatLng(41.5889352, -93.6171198),
+      new google.maps.LatLng(41.5887266, -93.6169696),
+      new google.maps.LatLng(41.587892, -93.616755),
+      new google.maps.LatLng(41.5860464, -93.6160254),
+      new google.maps.LatLng(41.585934, -93.6158538),
+      new google.maps.LatLng(41.5848587, -93.6155105),
+      new google.maps.LatLng(41.5828043, -93.6147594),
+      new google.maps.LatLng(41.582659899999996, -93.6145878),
+      new google.maps.LatLng(41.5821302, -93.6142445),
+      new google.maps.LatLng(41.5819215, -93.6139441),
+      new google.maps.LatLng(41.5810066, -93.612442),
+      new google.maps.LatLng(41.5802844, -93.6110044),
+      new google.maps.LatLng(41.5792089, -93.6082578),
+      new google.maps.LatLng(41.5787435, -93.6065626),
+      new google.maps.LatLng(41.5784706, -93.6062407),
+      new google.maps.LatLng(41.57845449999999, -93.6058116),
+      new google.maps.LatLng(41.577860599999994, -93.604331),
+      new google.maps.LatLng(41.5775717, -93.6033225),
+      new google.maps.LatLng(41.57668890000001, -93.600812),
+      new google.maps.LatLng(41.5766889, -93.6004901),
+      new google.maps.LatLng(41.5763999, -93.5994601),
+      new google.maps.LatLng(41.5759986, -93.5971427),
+      new google.maps.LatLng(41.59018700000001, -93.5969925),
+      new google.maps.LatLng(41.59581980000001, -93.5989666),
+      new google.maps.LatLng(41.5949532, -93.6032796),
+      new google.maps.LatLng(41.59484090000001, -93.6040306),
+      new google.maps.LatLng(41.5947767, -93.60467430000001),
+      new google.maps.LatLng(41.5947446, -93.6054683),
+      new google.maps.LatLng(41.5947767, -93.6065197),
+      new google.maps.LatLng(41.59533840000001, -93.6163902)
   ];
   neighborhoodPolygon = new google.maps.Polygon({
     path: hevCoords,
@@ -87,6 +116,16 @@ function initialize() {
       radius: 450,
       types: ['cafe', 'bar', 'night_club', 'lounge']
   };
+
+  // var hoodLayer = new google.maps.KmlLayer({
+  //   url: 'https://sites.google.com/site/hevdsm/kml/boundary.kml'
+  // }); 
+  // hoodLayer.setMap(map);
+
+  // var mapsEngineLayer = new google.maps.visualization.MapsEngineLayer({
+  //   layerId: '00463335882591727230-12798225287603138914',
+  //   map: map
+  // });
 
   placesService = new google.maps.places.PlacesService(map);
   infowindow = new google.maps.InfoWindow();
