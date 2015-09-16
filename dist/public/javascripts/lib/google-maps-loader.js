f0409aec6e12fd73ad7dd3f3deaafd3114bd6756
kPOWz <karrie.zeman@gmail.com>
2015-09-16 08:41:47 -0500

    bluebird reference must be passed into module

--- dist/public/javascripts/lib/google-maps-loader.js
+++ dist/public/javascripts/lib/google-maps-loader.js
@@ -1,23 +1,23 @@
 var google_maps_loaded_def = null;
 
-define(['bluebird.min'],function() {
+define(['bluebird.min'], function(P) {
   
   if(!google_maps_loaded_def) {
     
-  google_maps_loaded_def = P.defer();  //.pending() should be exposed by bluebird but erroring as undefined
+    google_maps_loaded_def = P.defer();  //.pending() should be exposed by bluebird
 
-  window.google_maps_loaded = function() {
-    google_maps_loaded_def.resolve(google.maps);  
+    window.google_maps_loaded = function() {
+      google_maps_loaded_def.resolve(google.maps);  
+    }
+
+    require(['http://maps.googleapis.com/maps/api/js?v=3.exp&callback=google_maps_loaded&key=AIzaSyBL0KrKrs7oKIS0Dlo3s9-ktwCt2iwCE1s&sensor=false&libraries=places,visualization']
+      ,function(){},function(err) {
+      google_maps_loaded_def.reject(err);
+        //throw err; // maybe freak out a little?
+    });
+
   }
 
-  require(['http://maps.googleapis.com/maps/api/js?v=3.exp&callback=google_maps_loaded&key=AIzaSyBL0KrKrs7oKIS0Dlo3s9-ktwCt2iwCE1s&sensor=false&libraries=places,visualization']
-    ,function(){},function(err) {
-    google_maps_loaded_def.reject(err);
-      //throw err; // maybe freak out a little?
-  });
-    
-  }
-  
   return google_maps_loaded_def.promise;
   
 });
